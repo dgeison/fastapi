6e54f5c425380111d6e650a55d95f23e7ec91412
dgeison <dgeison.peixoto@gmail.com>
2024-07-26 22:30:50 -0300

    até aqui com modelos de banco de dados

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,10 +14,21 @@ def client():
 
 @pytest.fixture()
 def session():
+    """
+    Creates a session with an in-memory SQLite database.
+
+    Returns:
+        Session: The created session object.
+
+    Raises:
+        None
+    """
     engine = create_engine('sqlite:///:memory:')
     table_registry.metadata.create_all(engine)
 
-    # live 43/151
+    # live 43/151 - explicação sobre with e yield
+    # with - gerenciamento de contexto
+    # yield - retorna o valor e continua a execução
     with Session(engine) as session:
         yield session
 
